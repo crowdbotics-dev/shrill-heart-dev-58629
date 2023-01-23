a964c84e615a13f8b82200c4233f06675f505109
Crowdbotics <engineering@crowdbotics.com>
2023-01-23 00:14:06 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "shrill-heart-dev-58629",
+  url: "https://shrill-heart-dev-58629.botics.co",
+  api: "https://shrill-heart-dev-58629.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
